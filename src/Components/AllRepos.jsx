0bf178550468f681d440dd999c0a72b29205de17
Ronnie Rios <ronniesmail14@gmail.com>
2023-01-27 12:09:42 -0600

    added initial files and react structre

---
+++ src/Components/AllRepos.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllRepos = () => {
+  return (
+    <div>AllRepos</div>
+  )
+}
+
+export default AllRepos
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AllRepos from '../Components/AllRepos'
+
+const HomePage = () => {
+  return (
+    <div>
+        <AllRepos />
+    </div>
+  )
+}
+
+export default HomePage
