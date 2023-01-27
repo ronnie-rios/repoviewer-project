6bdf067f36a5c3453e385f4f174ae44e021a5cb0
Ronnie Rios <ronniesmail14@gmail.com>
2023-01-27 12:41:59 -0600

    add loading component while fetch req is being processed

--- src/Components/AllRepos.jsx
+++ src/Components/AllRepos.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState} from 'react';
+import Loading from '../UI/Loading';
 
 const URL = process.env.REACT_APP_URL;
 const TOKEN = process.env.REACT_APP_TOKEN;
@@ -15,36 +16,40 @@ const AllRepos = () => {
         });
         const data = await response.json();
         setRepoData(data);
-        setLoading(false)
+        setLoading(false);
     };
    //TODO: sort data by star count
     useEffect(() => {
         getData()
     }, []);
-    
-  return (
-    <div>
-        {repoData.map((item) => {
-            const date = new Date(item.created_at)
-            return (
-                <div className='max-w-md p-6 m-6 border border-gray-800 rounded-lg shadow'key={item.id}> 
-                    <h1 className='mb-2 text-2xl font-bold tracking-tight'>Repo: {item.name.toUpperCase()}</h1>
-                    <p >Description: </p>
-                    <div>
-                        <div>
-                            <h3>{item.language}</h3>
 
-                            <h3>{item.stargazers_count} Stars</h3>
-                        
-                            <h3>{item.forks} Forks</h3>
+    if (loading) {
+        return <Loading />
+    } else {
+    return (
+        <div>
+            {repoData.map((item) => {
+                const date = new Date(item.created_at)
+                return (
+                    <div className='max-w-md p-6 m-6 border border-gray-800 rounded-lg shadow'key={item.id}> 
+                        <h1 className='mb-2 text-2xl font-bold tracking-tight'>Repo: {item.name.toUpperCase()}</h1>
+                        <p >Description: </p>
+                        <div>
+                            <div>
+                                <h3>{item.language}</h3>
+
+                                <h3>{item.stargazers_count} Stars</h3>
+                            
+                                <h3>{item.forks} Forks</h3>
+                            </div>
                         </div>
+                        <h3>Created at: {date.toLocaleDateString()}</h3>
                     </div>
-                    <h3>Created at: {date.toLocaleDateString()}</h3>
-                </div>
-            )
-        })}
-    </div>
-  )
+                )
+            })}
+        </div>
+        )
+    }
 }
 
 export default AllRepos
