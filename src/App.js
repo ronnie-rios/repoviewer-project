91bc48431d98bd896d8e2a9405d1772537b8004f
Ronnie Rios <ronniesmail14@gmail.com>
2023-01-27 13:11:57 -0600

    setup app.js with routing for components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import HomePage from './Pages/HomePage';
 import Navbar from './UI/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <HomePage />
-    </>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+      </Router>
   );
 }
 
