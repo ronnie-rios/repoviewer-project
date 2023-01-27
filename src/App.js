f5ede8aceb4ebd2ffa8d89b7e08a24ef192cc1e2
Ronnie Rios <ronniesmail14@gmail.com>
2023-01-27 12:19:16 -0600

    fetch request created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
     <>
+      <HomePage />
     </>
   );
 }
--- src/Components/AllRepos.jsx
+++ src/Components/AllRepos.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import { useEffect, useState} from 'react';
+
+const URL = process.env.REACT_APP_URL;
+const TOKEN = process.env.REACT_APP_TOKEN;
 
 const AllRepos = () => {
+    const [repoData, setRepoData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const getData = async () => {
+        const response = await fetch(URL, {
+            headers: {
+                'Authorization': TOKEN
+            }
+        });
+        const data = await response.json();
+        setRepoData(data);
+        setLoading(false)
+    };
+    console.log(repoData);
+    useEffect(() => {
+        getData()
+    }, [])
   return (
     <div>AllRepos</div>
   )
