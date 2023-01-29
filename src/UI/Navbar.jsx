eec0ecaf54946a89cbaf3bbb100fa14f8a10cbad
Ronnie Rios <ronniesmail14@gmail.com>
2023-01-28 20:10:21 -0600

    styling to navbar

--- src/UI/Navbar.jsx
+++ src/UI/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="bg-nav-color px-2 sm:px-4 py-2.5">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
         <ul className="flex flex-row p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 font-medium md:border-0">
-            <Link to='/'><li className="block py-2 pl-3 pr-4 text-white  md:p-0">Home</li></Link>
+            <Link to='/'><li className="block pl-3 pr-4"><button className="bg-btn-gray text-btn-text rounded p-4 border border-btn-gray hover:border-white">Home</button></li></Link>
         </ul>
       </div>
     </nav>
